ce40337fc365f8fe26637f67aaccfa14f192a3fc
sarina <sarinaomidvar99@gmail.com>
2023-07-08 02:02:14 +0430

    password reset added

--- core/core/settings.py
+++ core/core/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBckaend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
